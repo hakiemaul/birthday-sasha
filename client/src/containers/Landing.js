b3f3ad75c7c863bccaa244e14e6f47f3f84c4571
aulia hakim <auliahakim@aulias-MacBook-Air.local>
2017-08-03 20:52:39 +0700

    add landing collage and page

---
+++ client/src/containers/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Landing.css'
+
+export default class Landing extends React.Component {
+  render () {
+    return (
+      <div>
+        <article>
+          <h1 className="cover-heading">It's my birthday!</h1>
+        </article>
+      </div>
+    )
+  }
+}
