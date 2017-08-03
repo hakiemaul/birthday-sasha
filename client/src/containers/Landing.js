8b99e0916633f4582524bc020e65d4aad6f450ee
Aulia Hakiem <noersedya@gmail.com>
2017-08-03 22:28:00 +0700

    add conditional for birthday date

--- client/src/containers/Landing.js
+++ client/src/containers/Landing.js
@@ -3,11 +3,29 @@ import React from 'react'
 import './Landing.css'
 
 export default class Landing extends React.Component {
+  state = {
+    message: 'Is it birthday yet?'
+  }
+
+  _getFace () {
+    let dateNow = new Date()
+    let sashaBday = new Date("2017-08-10")
+    if (dateNow.getTime() >= sashaBday.getTime()) {
+      this.setState({
+        message: 'It sure is!'
+      })
+    } else {
+      this.setState({
+        message: 'Not yet :('
+      })
+    }
+  }
+
   render () {
     return (
       <div>
-        <article>
-          <h1 className="cover-heading">It's my birthday!</h1>
+        <article onClick={() => this._getFace()}>
+          <h1 className="cover-heading">{this.state.message}</h1>
         </article>
       </div>
     )
