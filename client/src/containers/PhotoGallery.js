bac1d9878491f30cee437f5ae3cc55462343f0b9
Aulia Hakiem <noersedya@gmail.com>
2017-08-07 10:00:56 +0700

    map carousel

--- client/src/containers/PhotoGallery.js
+++ client/src/containers/PhotoGallery.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Carousel, Image } from 'react-bootstrap'
 
 export default class PhotoGallery extends React.Component {
+  state = {
+    photos: [
+      {
+        uri: "/img/background-landing.png",
+        msg: "caption pertama"
+      },
+      {
+        uri: "/img/background-landing.png",
+        msg: "caption kedua"
+      },
+      {
+        uri: "/img/background-landing.png",
+        msg: "caption ketiga"
+      }
+    ]
+  }
+
   render () {
     return (
-      <div>
+      <div className="container">
+        <h1>Happy birthday to you, Sha.</h1>
+        <Carousel>
+          { this.state.photos.map(photo => (
+            <Carousel.Item>
+              <Image width={900} alt="900x500" src={photo.uri} responsive/>
+              <Carousel.Caption style={{ backgroundColor: 'white', color: 'black'}}>
+                <h3>{photo.msg}</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )) }
+        </Carousel>
       </div>
     )
   }
