518dd2849a34fc55eeff39f4dfb70e0c1d672ac5
hakiemaul <noersedya@gmail.com>
2017-08-09 23:32:24 +0700

    done

--- client/src/containers/Landing.js
+++ client/src/containers/Landing.js
@@ -4,16 +4,25 @@ import './Landing.css'
 
 export default class Landing extends React.Component {
   state = {
-    message: 'Is it birthday yet?'
+    message: 'Is it birthday yet?',
+    countdown: 4
   }
 
   _getFace () {
     let dateNow = new Date()
     let sashaBday = new Date("2017-08-10")
-    if (dateNow.getTime() < sashaBday.getTime()) {
+    if (dateNow.getTime() >= sashaBday.getTime()) {
       this.setState({
-        message: 'It sure is!'
+        message: 'It sure is! Early present in'
       })
+      setInterval(() => {
+        this.setState({
+          countdown: this.state.countdown - 1
+        })
+      }, 1000)
+      setTimeout(() => {
+        this.props.history.push('/showtime')
+      }, 4000)
     } else {
       this.setState({
         message: 'Not yet :('
@@ -25,7 +34,7 @@ export default class Landing extends React.Component {
     return (
       <div>
         <article onClick={() => this._getFace()}>
-          <h1 className="cover-heading">{this.state.message}</h1>
+          { this.state.countdown > 3 ? <h1 className="cover-heading">{this.state.message}</h1> : <h1 className="cover-heading">{this.state.message + ' ' + this.state.countdown}</h1>}
         </article>
       </div>
     )
--- client/src/containers/PhotoGallery.js
+++ client/src/containers/PhotoGallery.js
@@ -5,35 +5,48 @@ export default class PhotoGallery extends React.Component {
   state = {
     photos: [
       {
-        uri: "/img/background-landing.png",
-        msg: "caption pertama"
+        uri: "/img/carousel-sasha.jpg",
+        msg: "May you're granted with health and success.",
+        msg2: "Keep smiling and shine the world with it..."
       },
       {
-        uri: "/img/background-landing.png",
-        msg: "caption kedua"
+        uri: "/img/carousel-teman.jpg",
+        msg: "May you're granted a lasting friendships.",
+        msg2: "Be grateful to know that your friends will always be there for you..."
       },
       {
-        uri: "/img/background-landing.png",
-        msg: "caption ketiga"
+        uri: "/img/carousel-keluarga.jpg",
+        msg: "May your family is blessed and loved by Allah SWT.",
+        msg2: "Be grateful that your family loves you as much as you love them..."
+      },
+      {
+        uri: "/img/carousel-hakiem.jpg",
+        msg: "Happy 21st birthday.",
+        msg2: "From your annoying faraway boyfriend, who will always love you.",
+        side: "PS: Sorry I'm not there.. :(",
+        side2: "PS2: Real present in our next meeting! ;)"
       }
     ]
   }
 
   render () {
     return (
-      <div className="container">
-        <h1>Happy birthday to you, Sha.</h1>
         <Carousel>
-          { this.state.photos.map(photo => (
-            <Carousel.Item>
-              <Image width={900} alt="900x500" src={photo.uri} responsive/>
-              <Carousel.Caption style={{ backgroundColor: 'white', color: 'black'}}>
+          { this.state.photos.map((photo, index) => (
+            <Carousel.Item key={index}>
+              <Image alt="900x500" src={photo.uri}/>
+              <Carousel.Caption style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)', color: 'black', padding: 40}}>
+                <h1>Happy birthday to you...</h1>
                 <h3>{photo.msg}</h3>
+                <h3>{photo.msg2}</h3>
+                { photo.side ? (<div>
+                    <h5>{photo.side}</h5>
+                    <h5>{photo.side2}</h5>
+                  </div>) : ''}
               </Carousel.Caption>
             </Carousel.Item>
           )) }
         </Carousel>
-      </div>
     )
   }
 }
